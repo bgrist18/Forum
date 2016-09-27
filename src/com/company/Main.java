0291e8868b004948d02120364d12d9ff45d97060
bgrist18 <bgrist309@gmail.com>
2016-09-26 21:13:31 -0400

    import

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.company.Post;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -8,9 +10,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<Post> posts = new ArrayList<>();
-
         File f = new File("posts.txt");
+        ArrayList<Post> posts = loadPosts(f);
+
+        Scanner consoleScanner = new Scanner(System.in);
+
+        int replyId = -1;
+        while (true) {
+            printPosts(posts, replyId);
+
+            replyId = readId(consoleScanner.nextLine());
+        }
+    }
+
+    public static ArrayList<Post> loadPosts(File f) throws FileNotFoundException {
+        ArrayList<Post> posts = new ArrayList<>();
         Scanner fileScanner = new Scanner(f);
         while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
@@ -21,20 +35,21 @@ public class Main {
             Post p = new Post(Integer.valueOf(replyId), author, text);
             posts.add(p);
         }
+        return posts;
+    }
 
-        Scanner consoleScanner = new Scanner(System.in);
-
-        int replyId = -1;
-        while (true) {
-            System.out.println("Posts replying to " + replyId);
-            for (int i = 0; i < posts.size(); i++) {
-                Post post = posts.get(i);
-                if (post.replyId == replyId) {
-                    System.out.printf("[%s] %s by %s\n", i , post.text, post.author);
-                }
+    public static void printPosts(ArrayList<Post> posts, int replyId) {
+        System.out.println("Posts replying to " + replyId);
+        for (int i = 0; i < posts.size(); i++) {
+            Post post = posts.get(i);
+            if (post.replyId == replyId) {
+                System.out.printf("[%s] %s by %s\n", i, post.text, post.author);
             }
-            System.out.println("Type the id you want to see for replies");
-            replyId = Integer.valueOf(consoleScanner.nextLine());
         }
+    }
+
+    public static int readId(String input) {
+        System.out.println("Type the id you want to see replies to:");
+        return Integer.valueOf(input);
     }
 }
--- src/com/company/Post.java
+++ src/com/company/Post.java
@@ -4,9 +4,9 @@ package com.company;
  * Created by brettgrist on 9/22/16.
  */
 public class Post {
-    int replyId;
-    String author;
-    String text;
+    public int replyId;
+    public String author;
+    public String text;
 
     public Post(int replyId, String author, String text) {
         this.replyId = replyId;
